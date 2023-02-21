0f89a546fcfa55546832b050229118e945ac34df
sgossmann <sandra.gossmann4@gmail.com>
2023-02-21 16:25:24 +0100

    modif SerieController.php + requirements dans méthode show + show.html.twig

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -6,19 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/serie', name: 'serie_')]
 class SerieController extends AbstractController
 {
-    #[Route('/serie/list', name: 'serie_list')]
+    #[Route('/list', name: 'list')]
     public function list(): Response
     {
         //TODO récupérer la liste des séries en bdd
-        return $this->render('serie/list.html.twig', );
+        return $this->render('serie/list.html.twig');
     }
 
-    #[Route('/serie/add', name: 'serie_add')]
+    #[Route('/add', name: 'add')]
     public function add(): Response
     {
         //TODO créer un formulaire d'ajout de série
-        return $this->render('serie/add.html.twig', );
+        return $this->render('serie/add.html.twig');
     }
+
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    public function show(int $id): Response
+    {
+        dump($id);
+        //TODO récupérer les infos de la série
+        return $this->render('serie/show.html.twig');
+    }
+
 }
