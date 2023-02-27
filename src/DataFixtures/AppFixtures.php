ee9b7cfd378b4037d2cf75fde668e7dc603ae07e
sgossmann <sandra.gossmann4@gmail.com>
2023-02-27 10:07:26 +0100

    fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        $this->addSeries($manager, $faker);
+    }
+
+    public function addSeries(ObjectManager $manager, Generator $generator){
+        for ($i=0; $i<50; $i++){
+            $serie = new Serie();
+            $serie
+                ->setName(implode($generator->words(3)))
+                ->setVote($generator->numberBetween(0, 10))
+                ->setStatus($generator->randomElement(["ended", "returning", "canceled"]))
+                ->setPoster("poster.png")
+                ->setTmdbId(123)
+                ->setPopularity("150.3")
+                ->setFirstAirDate($generator->dateTimeBetween("-6 months"))
+                ->setLastAirDate($generator->dateTimeBetween($serie->getFirstAirDate()))
+                ->setGenres($generator->randomElement(['Comedy', 'Western', 'Drama']))
+                ->setBackdrop("backdrop.png");
+            $manager->persist($serie);
+        }
+        $manager->flush();
+    }
+}
