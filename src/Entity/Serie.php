558d5cc08d266e86e6a99877a2a390c62802262b
sgossmann <sandra.gossmann4@gmail.com>
2023-02-27 16:30:41 +0100

    ajout bouton delete + cascade sur season dans Serie.php

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -72,7 +72,7 @@ class Serie
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $dateModified = null;
 
-    #[ORM\OneToMany(mappedBy: 'serie', targetEntity: Season::class)]
+    #[ORM\OneToMany(mappedBy: 'serie', targetEntity: Season::class, cascade: ["remove", "persist"])]
     private Collection $seasons;
 
     public function __construct()
